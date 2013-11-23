9b041b3223a22f747639c730f59464ae28867410
Florian Asche <>
2013-11-23 01:09:21 +0100

    removed BOOT from D output

--- rfmsrc/OpenHR20/com.c
+++ rfmsrc/OpenHR20/com.c
@@ -291,11 +291,7 @@ void COM_print_debug(uint8_t type) {
 	print_s_p(PSTR(" I: "));
 	print_decXXXX(temp_average);
 	print_s_p(PSTR(" S: "));
-	if (CTL_temp_wanted_last>TEMP_MAX+1) {
-		print_s_p(PSTR("BOOT"));
-	} else {
-		print_decXXXX(calc_temp(CTL_temp_wanted_last));
-	}
+	print_decXXXX(calc_temp(CTL_temp_wanted_last));
 	print_s_p(PSTR(" B: "));
 	print_decXXXX(bat_average);
 #if DEBUG_PRINT_I_SUM
